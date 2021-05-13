42946f4adb72d46875b7f500cf14b53f8b41da7e
Siddhant N Trivedi <68370997+siddhant-deepsource@users.noreply.github.com>
2021-05-13 16:28:11 +0530

    Change formatting

--- demo_code.py
+++ demo_code.py
@@ -82,10 +82,7 @@ def tar_something():
 def bad_isinstance(initial_condition, object, other_obj, foo, bar, baz):
     if (
         initial_condition
-        and (
-            isinstance(object, int)
-            or isinstance(object, float)
-            or isinstance(object, str)
+        and (isinstance(object, int) or isinstance(object, float) or isinstance(object, str)
         )
         and isinstance(other_obj, float)
         and isinstance(foo, str)
