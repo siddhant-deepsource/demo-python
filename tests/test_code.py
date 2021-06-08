d9a3bd762bf61cd341bcc9d792e8161d8781ea4a
Siddhant N Trivedi <68370997+siddhant-deepsource@users.noreply.github.com>
2021-06-08 19:45:10 +0530

    Update test_code.py

--- tests/test_code.py
+++ tests/test_code.py
@@ -2,5 +2,5 @@ from demo_code import RandomNumberGenerator
 
 
 def test_random_number_generator():
-    """Test random number generator."""
+    """Test random number genera"""
     assert RandomNumberGenerator().get_number()
